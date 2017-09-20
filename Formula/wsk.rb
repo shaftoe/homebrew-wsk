89826cb6447c9ce53bd2d3ecf83b383dec9b6f2d
Alexander Fortin <alexander.fortin@gmail.com>
2017-09-20 22:05:13 +0200

    Add new url

--- Formula/wsk.rb
+++ Formula/wsk.rb
@@ -1,7 +1,7 @@
 class Wsk < Formula
   desc "OpenWhisk CLI"
   homepage "https://console.ng.bluemix.net/openwhisk/cli"
-  url "https://openwhisk.ng.bluemix.net/cli/go/download/mac/amd64/OpenWhisk_CLI-mac.zip"
+  url "https://openwhisk.eu-gb.bluemix.net/cli/go/download/mac/amd64/OpenWhisk_CLI-mac.zip"
   version "0"
 
   def install
